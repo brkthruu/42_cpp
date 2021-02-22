81f9cd35be091007b3a2ee146e32703dd0526d4c
hysimok <jeonghy95@gmail.com>
2021-02-22 17:01:32 +0900

    add compile option

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,21 +6,21 @@
 /*   By: hjung <hjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 14:08:35 by hjung             #+#    #+#             */
-/*   Updated: 2021/02/16 14:11:01 by hjung            ###   ########.fr       */
+/*   Updated: 2021/02/22 17:00:56 by hjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-char	ft_toupper(char c)
+char 	ft_toupper(char c)
 {
 	return ((c >= 'a' && c <= 'z') ? c - ('a' - 'A') : c);
 }
 
-int		main(int argc, char *argv[])
+int 	main(int argc, char *argv[])
 {
-	int		i;
-	int		j;
+	int i;
+	int j;
 
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
