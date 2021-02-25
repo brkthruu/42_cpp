249c52a956dcf5cde2247359c36416845a0dcea7
hysimok <jeonghy95@gmail.com>
2021-02-26 05:49:22 +0900

    pointer and reference

---
+++ CPP01/ex04/ex04.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex04.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hjung <hjung@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/26 05:41:55 by hjung             #+#    #+#             */
+/*   Updated: 2021/02/26 05:49:05 by hjung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+
+int	main()
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string		*ptr = &str;
+	std::string		&ref = str;
+
+	std::cout << std::setw(13) << "[ORIGIN] " << str << " :: " << &str << '\n';
+	std::cout << std::setw(13) << "[Pointer] " << *ptr << " :: " << &ptr << '\n';
+	std::cout << std::setw(13) << "[Reference] " << ref << " :: " << &ref << '\n';
+}
